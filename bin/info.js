ed25d7969182ed00d7314b6d767d8e02d102df9a
Vasiliy Ivanov <vasilisko@gmail.com>
2021-03-11 09:14:01 +0300

    fix package.json

--- bin/info.js
+++ bin/info.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 
+const date = require('date-and-time');
 const info = require('./../package.json');
 
 console.info(`date            - ${date.format(new Date(), 'DD.MM.YYYY HH:mm:ss')}`);
 console.info(`description     - ${info.description}`);
 console.info(`author          - ${info.author.name}`);
 console.info(`email           - ${info.author.email}`);
-console.info(`stand           - ${type}`);
-console.info(`version/branch  - ${branch}`);
 console.info(`project version - ${info.version}`);
